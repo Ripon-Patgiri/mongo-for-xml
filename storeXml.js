dbb956175abfbd4a82ad63255409e826c0a7d04e
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-17 19:17:50 +0530

    Imported Required Modules, Read XML Data and converted to JS Object using xml2js

--- storeXml.js
+++ storeXml.js
@@ -2,3 +2,18 @@
 const fs = require("fs");
 const xml2js = require("xml2js");
 const MongoClient = require("mongodb").MongoClient;
+
+// Read the XML Data
+const xmlData = fs.readFileSync("./bookstore.xml");
+
+// Use xml2js module to convert XML data to a JavaScript Object
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Connected to MongoDB");
+  }
+});
