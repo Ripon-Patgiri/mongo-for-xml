4e6e76b0de148e5bada8efae097554f34dee35c7
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-19 19:31:41 +0530

    Updated Import Syntax , fixed connection to db using async function, updated db name and collection name.

--- storeXml.js
+++ storeXml.js
@@ -1,13 +1,13 @@
 // Import Required Modules
-const fs = require("fs");
-const xml2js = require("xml2js");
-const MongoClient = require("mongodb").MongoClient;
+import { readFileSync } from "fs";
+import { parseString } from "xml2js";
+import { MongoClient } from "mongodb";
 
 // Read the XML Data
-const xmlData = fs.readFileSync("./bookstore.xml");
+const xmlData = readFileSync("./bookstore.xml");
 
 // Use xml2js module to convert XML data to a JavaScript Object
-const jsData = xml2js.parseString(xmlData, (err, result) => {
+const jsData = parseString(xmlData, (err, result) => {
   if (err) {
     console.error(err);
   } else {
@@ -17,19 +17,21 @@ const jsData = xml2js.parseString(xmlData, (err, result) => {
 
 // Connect to MongoDB Database
 const url = "mongodb://localhost:27017";
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-client.connect((err) => {
-  if (err) {
+let client;
+async function connectToDatabase() {
+  client = new MongoClient(url);
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+  } catch (err) {
     console.error(err);
-  } else {
-    console.log("Connected to MongoDB");
   }
-});
+}
+connectToDatabase();
 
 // Insert Data into a Collection in Database
-const db = client.db("dbname");
-const collection = db.collection("collectionname");
+const db = client.db("adbd_project");
+const collection = db.collection("adbd_xml");
 
 collection.insertOne(jsData, (err, result) => {
   if (err) {
