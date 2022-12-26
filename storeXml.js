f8f4825a3e16848a3c62f44be6ca650b5a5559af
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-26 18:35:05 +0530

    Fixed storing xml data to mongodb.

--- storeXml.js
+++ storeXml.js
@@ -1,36 +1,31 @@
-// Import xml2js  library
-const xml2js = require('xml2js');
+const { MongoClient } = require("mongodb");
+const xml2js = require("xml2js");
+const fs = require("fs");
 
-// Read XML File and Parse it using 'xml2js' library
-const fs = require('fs');
-
-fs.readFile('movies.xml', (err, data) => {
-  if (err) throw err;
-
-  xml2js.parseString(data, (err, result) => {
-    if (err) throw err;
-
-    console.log(result);
+async function main() {
+  const uri =
+    "mongodb+srv://ripon_p:ripon123@cluster0.zf3lzix.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
-});
 
-// Using JSON.stringify() to convert the object to a JSON Document
-const json = JSON.stringify(result);
+  try {
+    await client.connect();
+    const xml = await fs.promises.readFile("movies.xml");
+    const obj = await xml2js.parseStringPromise(xml);
+    const collection = client.db("adbd").collection("xml_movies");
+    const result = await collection.insertOne(obj);
+    if (result.acknowledged === true) {
+      console.log(`XML Document was successfully inserted with the _id: ${result.insertedId}`);
+    } else {
+      console.log("There was an error inserting the data into the database");
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
 
-// Connect to a MongoDB Server
-const MongoClient = require('mongodb').MongoClient;
-
-const url = 'mongodb://localhost:27017';
-const dbName = 'myproject';
-
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) throw err;
-
-  console.log('Connected to MongoDB server');
-
-  const db = client.db(dbName);
-
-  // Perform operations on the database here
-
-  client.close();
-});
+main().catch(console.error);
